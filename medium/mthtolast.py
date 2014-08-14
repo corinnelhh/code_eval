9225cbb7bf843a836f6ed61356331115a28d92a3
corinnelhh <corinne.hutchinson@gmail.com>
2014-08-14 16:05:28 -0700

    added mth_to_last description

--- medium/mthtolast.py
+++ medium/mthtolast.py
@@ -1,3 +1,27 @@
+# https://www.codeeval.com/open_challenges/10/
+
+# Mth to last element
+# Challenge Description:
+
+# Write a program to determine the Mth to last element of a list.
+# Input sample:
+
+# The first argument will be a path to a filename containing a series of
+# space delimited characters followed by an integer representing a index
+# into the list (1 based), one per line. E.g.
+
+# a b c d 4
+# e f g h 2
+
+# Output sample:
+
+# Print to stdout, the Mth element from the end of the list, one per line.
+# If the index is larger than the list size, ignore that input. E.g.
+
+# a
+# g
+
+
 import sys
 
 
