2820ca2744d657f2d00043fc2915dfc6a86188a8
corinnelhh <corinne.hutchinson@gmail.com>
2014-08-14 16:01:10 -0700

    added prime_palindromes description

--- easy/prime_palindromes.py
+++ easy/prime_palindromes.py
@@ -1,3 +1,16 @@
+# https://www.codeeval.com/open_challenges/3/
+
+# Write a program to determine the biggest prime palindrome under 1000.
+# Input sample:
+
+# There is no input for this program.
+# Output sample:
+
+# Your program should print the largest palindrome on stdout, i.e.
+
+# 929
+
+
 def prime_palindromes(n):
     nums = [i for i in range(2, n + 1)]
     primes = sieve_of_eratosthenes(nums, 2)
