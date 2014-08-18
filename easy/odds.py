313adc496954249d5afb7a2ea7c7bcadef7316ea
corinnelhh <corinne.hutchinson@gmail.com>
2014-08-18 10:52:22 -0700

    added odds printing

---
+++ easy/odds.py
@@ -0,0 +1,15 @@
+# https://www.codeeval.com/open_challenges/25/
+
+# Odd Numbers
+# Challenge Description:
+
+# Print the odd numbers from 1 to 99.
+# Input sample:
+
+# There is no input for this program.
+# Output sample:
+
+# Print the odd numbers from 1 to 99, one number per line.
+
+for num in range(1, 100, 2):
+    print num
