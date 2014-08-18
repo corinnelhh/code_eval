72f155f0b2a3ea0979fa232a43f45bd4a3b508f7
corinnelhh <corinne.hutchinson@gmail.com>
2014-08-17 21:45:23 -0700

    added lowercase challenge

---
+++ easy/lowercase.py
@@ -0,0 +1,27 @@
+# https://www.codeeval.com/open_challenges/20/
+
+# Given a string write a program to convert it into lowercase.
+# Input sample:
+
+# The first argument will be a path to a filename containing sentences,
+# one per line. You can assume all characters are from the english language.
+# E.g.
+
+# HELLO CODEEVAL
+# This is some text
+
+# Output sample:
+
+# Print to stdout, the lowercase version of the sentence, each on a new line.
+# E.g.
+
+# hello codeeval
+# this is some text
+
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    lines = f.readlines()
+
+for line in lines:
+    print line.lower()
