b081a3f8c99926fa555e807d743cb84c66fb118a
corinnelhh <corinne.hutchinson@gmail.com>
2014-08-25 11:02:46 -0400

    added endianness challenge

---
+++ medium/endianness.py
@@ -0,0 +1,24 @@
+# https://www.codeeval.com/open_challenges/15/
+
+# Endianness
+# Challenge Description:
+
+# Write a program to print out the endianness of the system.
+# Input sample:
+
+# None
+# Output sample:
+
+# Print to stdout, the endianness, wheather it is LittleEndian or BigEndian.
+# e.g.
+
+# BigEndian
+
+import sys
+
+endian = sys.byteorder
+
+if endian == 'little':
+    print "LittleEndian"
+else:
+    print "BigEndian"
