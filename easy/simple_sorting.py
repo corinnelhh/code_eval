bbf898dd46cdff9b870c092a321ac42a89622fc8
corinnelhh <corinne.hutchinson@gmail.com>
2014-08-26 09:10:36 -0700

    added simple sorting challenge

---
+++ easy/simple_sorting.py
@@ -0,0 +1,29 @@
+# https://www.codeeval.com/open_challenges/91/
+
+# Simple Sorting
+# Challenge Description:
+
+# Write a program which sorts numbers.
+# Input sample:
+
+# Your program should accept as its first argument a path to a filename.
+# Input example is the following
+
+# 70.920 -38.797 14.354 99.323 90.374 7.581
+# -37.507 -3.263 40.079 27.999 65.213 -55.552
+
+# Output sample:
+
+# Print sorted numbers in the following way. Please note, that you need
+# to print the numbers till the 3rd digit after the dot including trailing
+# zeros.
+
+# -38.797 7.581 14.354 70.920 90.374 99.323
+# -55.552 -37.507 -3.263 27.999 40.079 65.213
+
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    for line in f.readlines():
+        print " ".join(["{0:.3f}".format(num) for num in sorted(
+            [float(num) for num in line.strip().split()])])
